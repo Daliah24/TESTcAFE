b3024b19411573f25e6201d76198b59ee17b6c0c
Daliah24 <amante.denmark@ncst.edu.ph>
2024-06-21 15:39:37 +0800

    Some Fixes on The href nav, added  hamburger menu functionality

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var toggleButton = document.getElementById('toggle-menu');
-  var menuIcon = document.getElementById('menu-icon');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const menuToggle = document.getElementById('menu-icon');
+  const nav = document.querySelector('.navbar');
 
-  toggleButton.addEventListener('click', function() {
-    // Check if the menu icon is currently displayed
-    if (menuIcon.style.display === 'none' || menuIcon.style.display === '') {
-      menuIcon.style.display = 'block'; // Show the menu icon
-    } else {
-      menuIcon.style.display = 'none'; // Hide the menu icon
-    }
+  menuToggle.addEventListener('click', () => {
+      nav.classList.toggle('open');
   });
 });
