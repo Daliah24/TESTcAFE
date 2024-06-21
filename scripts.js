d176d36bb98363103eb04f6ad1f6c60d5a137cc6
Daliah24 <amante.denmark@ncst.edu.ph>
2024-06-21 11:32:00 +0800

    HAHAHAHAAHAHA

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var toggleButton = document.getElementById('toggle-menu');
+  var menuIcon = document.getElementById('menu-icon');
+
+  toggleButton.addEventListener('click', function() {
+    // Check if the menu icon is currently displayed
+    if (menuIcon.style.display === 'none' || menuIcon.style.display === '') {
+      menuIcon.style.display = 'block'; // Show the menu icon
+    } else {
+      menuIcon.style.display = 'none'; // Hide the menu icon
+    }
+  });
+});
