06010e1c5680a7d3831ba3b90465245659ddfbd9
Macabonk <122765170+Macabonk@users.noreply.github.com>
2024-06-22 09:25:48 +0800

    Added Second Rate Shaboingery

--- scripts.js
+++ scripts.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
   });
 });
 
+
+//Second Rate Shaboingery
+function areYouSure(){
+  let i = 'Are You Sure?'
+  if (confirm(i)){
+    window.open('https://www.youtube.com/watch?v=QwLvrnlfdNo', '_blank').focus();
+  }
+  else{
+    alert('Okay! :D');
+  }
+}
