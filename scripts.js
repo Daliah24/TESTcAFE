916144fbb9815106fe1a01eed695edb082121aef
Daliah24 <amante.denmark@ncst.edu.ph>
2024-06-23 21:34:03 +0800

    ito na ferry

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const menuToggle = document.getElementById('menu-icon');
   const nav = document.querySelector('.navbar');
+  const menuItems = document.querySelectorAll('.navbar .menu-item'); // Ensure this correctly selects your menu items
 
   menuToggle.addEventListener('click', () => {
-      nav.classList.toggle('open');
+    nav.classList.toggle('open');
+  });
+
+  // Add click event listener to each menu item
+  menuItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+      event.stopPropagation(); // Prevent event from propagating
+      // Close the navbar menu when a menu item is clicked
+      nav.classList.remove('open');
+    });
   });
 });
-
 
 //Second Rate Shaboingery
 function areYouSure(){
